04d615bdb3e67e072dd4c995c29b1abc60c75e2d
Uria <uria.webdev@gmail.com>
2023-09-08 10:48:42 +0200

    add components

---
+++ src/components/creation_1.vue
@@ -0,0 +1,9 @@
+<template>
+    <section class="section">
+        <h2>
+            Première création,<br>
+            Création d'un CV en HTML et CSS :
+        </h2>
+        <img src="../assets/img/presentation-CV.png" alt="Image de présentation du CV" class="présentation">
+    </section>
+</template>
